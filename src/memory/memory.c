c5b5ef768c90fd255ceded23e993b4132f45983f
Alexandr Chudopal <alex.ch@core-tech.ru>
2020-10-20 21:26:58 +0300

    added write

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -9,6 +9,7 @@ void defragmentation();
 
 struct virtual_memory memory;
 
+//-------------------------------------------------------------------------------------------------
 m_id m_malloc(int size_of_chunk, m_err_code* error) {
     
     m_id segment = find_free_segment(size_of_chunk);
@@ -23,7 +24,7 @@ m_id m_malloc(int size_of_chunk, m_err_code* error) {
     }
 }
 
-
+//-------------------------------------------------------------------------------------------------
 m_id find_free_segment(int size_of_chunk){
     for (int i = 0; i < memory.number_of_pages; i++){
         bool is_oversize = false;
@@ -63,22 +64,24 @@ m_id find_free_segment(int size_of_chunk){
     return NULL;
 }
 
-
+//-------------------------------------------------------------------------------------------------
 void m_free(m_id ptr, m_err_code* error) {
-    
+    m_id -> is_used = false;
 }
 
-
+//-------------------------------------------------------------------------------------------------
 void m_read(m_id read_from_id,void* read_to_buffer, int size_to_read, m_err_code* error) {
-    
+    memcpy(read_to_buffer, read_from_id, size_to_read);
+    *error = M_ERR_OK;
 }
 
-
+//-------------------------------------------------------------------------------------------------
 void m_write(m_id write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error) {
-    
+    memcpy(write_to_id, write_from_buffer, size_to_write);
+    *error = M_ERR_OK;
 }
 
-
+//-------------------------------------------------------------------------------------------------
 void m_init(int number_of_pages, int size_of_page, int temporary_locality) {
     
     struct page_frame * pages;
@@ -98,6 +101,7 @@ void m_init(int number_of_pages, int size_of_page, int temporary_locality) {
     memory.number_of_pages = number_of_pages;
 }
 
+//-------------------------------------------------------------------------------------------------
 void defragmentation(){
     for (int i = 0; i < memory.number_of_pages; i++){//страницы
         
